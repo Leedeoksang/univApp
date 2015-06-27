1536a2d59b87e66332aa0def8e0077a2f55a96e1
Leedeoksang <duksang4834@postech.ac.kr>
2015-06-27 22:47:07 +0900

    temp

---
+++ app/scripts/controllers/mainController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name UnivApp.controller:MainController
+ * @description
+ * # MainController
+ */
+angular.module('UnivApp')
+  .controller('MainController', function($scope) {
+
+    // do something with $scope
+
+  });
---
+++ app/scripts/controllers/settingsController.js
@@ -0,0 +1,14 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name UnivApp.controller:SettingsController
+ * @description
+ * # SettingsController
+ */
+angular.module('UnivApp')
+  .controller('SettingsController', function($scope) {
+
+    // do something with $scope
+
+  });
